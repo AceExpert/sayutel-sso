e57c5cf8248d5300e785aa2e596ca7c5aad69cd0
sputh-the-pigeon <spuththepigeon@gmail.com>
2025-11-10 09:37:23 +0530

    added normal text/plain with text/html for contact email

--- src/extensions/shaleen/index.js
+++ src/extensions/shaleen/index.js
@@ -9,6 +9,7 @@ function sendMail({name, email, message}) {
         '--to-name', 'Shaleen Singh', 
         '-s', `Contact Request from ${name} through shaleen.net`,
         '--html', contactHTML(name, email, message),
+        '-m', `Dear Shaleen\nSomeone named ${name} has sent you a message through your website to contact you. The details are given below.\n\nYou can directly reply to this email to reply to them.\n\nName:\n${name}\n\nEmail:\n${email}\n\nMessage:\n${message}\n\n__________________`,
         '--name', 'shaleen.net',
         '--sign', 'sayutel.com',
         '--no-save'
@@ -20,6 +21,7 @@ function sendMail({name, email, message}) {
         '--to-name', name, 
         '-s', `Contact Request Sent from ${name} to Shaleen`,
         '--html', contactRHTML(name, email, message),
+        '-m', `Dear ${name}\nYour message has been sent to Shaleen in her mail. Below is a copy of your responses.\n\nThis email is just a receipt. Do not reply to it.\n\nName:\n${name}\n\nEmail:\n${email}\n\nMessage:\n${message}\n\n__________________`,
         '--name', 'shaleen.net',
         '--sign', 'sayutel.com',
         '--no-save'
